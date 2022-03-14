c8a513e80b9ff7201803b2694f441d2ea8503847
JuqiPeng <pengjuqi@163.com>
2022-03-14 09:55:05 +0800

    Add PyMuPDF  module to realize the conversion from PDF to imagee

--- convert_app/convert/pdf_to_img.py
+++ convert_app/convert/pdf_to_img.py
@@ -3,6 +3,7 @@ import tempfile
 from convert_app.utils import zip_folder
 from pdf2image import convert_from_path
 from django.conf import settings
+import fitz
 
 
 class PdfToImage:
@@ -14,9 +15,24 @@ class PdfToImage:
         self.filename = f"{settings.MEDIA_ROOT}/{filename}"
         self.image_path = image_path
     
+    def convert_by_pymupdf(self):
+        if not os.path.exists(self.image_path):
+            os.mkdir(self.image_path)
+        doc = fitz.open(self.filename)
+        for page in doc:
+            pix = page.get_pixmap(alpha = False)
+            pix.save(f"{self.image_path}/{page.number}.png")
+        doc.close()
+    
     def convert(self):
         with tempfile.TemporaryDirectory() as path:
-            images = convert_from_path(self.filename, output_folder=path, dpi=200)
+            images = convert_from_path(
+                self.filename, 
+                output_folder=path, 
+                dpi=200,
+                fmt='png',
+                thread_count=4
+            )
             for index, image in enumerate(images):
                 if not os.path.exists(self.image_path):
                     os.makedirs(self.image_path)
@@ -30,6 +46,6 @@ class PdfToImage:
         shutil.rmtree(self.image_path)
 
     def handle(self, dest_name: str):
-        self.convert()
+        self.convert_by_pymupdf()
         self.zip_image(dest_name)
         self.clear()
